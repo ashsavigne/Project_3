cda2aa515cabf5a3c6e6ce464276bc0a55e6b298
LoganSikes <92864503+LoganSikes@users.noreply.github.com>
2023-08-03 13:32:12 -0400

    Create song.h

---
+++ song.h
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+struct Song {
+    // class variables
+    string name;
+    string album;
+    string artist;
+    bool exp;
+    int dance;
+    int key;
+    int energy;
+    int loud;
+    int year;
+    int tempo;
+    int valence;
+    int rating;
+public:
+    void Deserialize(istringstream& stream);
+};
+
+class Playlist{
+public:
+    Song* s;
+    vector<Song*> songs;
+    void TestPrint(){
+        cout << songs.size();
+    };
+    Playlist() : songs(){}
+    void Insert(Song* song, const string& option, const string& adj, bool exp, int year, const string& artist);
+    vector<int> Ratings();
+    void PrintAll();
+    void PrintN(int n);
+    ~Playlist();
+};
